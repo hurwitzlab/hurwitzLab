efdd5bd44472a7547622933639d43cd1a89a91b1
Kenneth Schackart <schackartk1@gmail.com>
2021-09-12 16:56:16 -0700

    reformat to pass linting

--- R/pal.R
+++ R/pal.R
@@ -1,15 +1,15 @@
 hurwitz_colors <- c(
-  'orange' = "#F5811F",
-  'teal' = "#018D97",
-  'gold' = "#FFD900",
-  'viking' = "#61CDDC",
-  'dark_gray' = "#6A6A6A",
-  'red' = "#8B3726",
-  'tan' = "#D2B48C",
-  'cyan' = "#97FFFF",
-  'charcoal' = "#333333",
-  'light_gray' = "#CCCCCC",
-  'brown' = "#8B7355"
+  "orange" = "#F5811F",
+  "teal" = "#018D97",
+  "gold" = "#FFD900",
+  "viking" = "#61CDDC",
+  "dark_gray" = "#6A6A6A",
+  "red" = "#8B3726",
+  "tan" = "#D2B48C",
+  "cyan" = "#97FFFF",
+  "charcoal" = "#333333",
+  "light_gray" = "#CCCCCC",
+  "brown" = "#8B7355"
 )
 
 #' Extract colors as hex codes
@@ -21,7 +21,7 @@ get_hurwitz_colors <- function(...) {
   colors <- c(...)
 
   if (is.null(colors))
-    return (hurwitz_colors)
+    return(hurwitz_colors)
 
   colors <- stringr::str_replace(colors, "grey", "gray")
 
@@ -43,17 +43,17 @@ get_hurwitz_colors <- function(...) {
 get_hurwitz_colours <- get_hurwitz_colors
 
 hurwitz_palettes <- list(
-  'all' = get_hurwitz_colors("orange", "teal", "gold", "viking",
+  "all" = get_hurwitz_colors("orange", "teal", "gold", "viking",
                              "dark_gray", "red", "tan", "cyan",
                              "charcoal", "light_gray", "brown"),
-  'main'  = get_hurwitz_colors("teal", "dark_gray", "orange"),
-  'classic'  = get_hurwitz_colors("teal", "orange"),
-  'gray'  = get_hurwitz_colors("light_gray", "dark_gray"),
-  'distinguish' = get_hurwitz_colors("teal", "gold", "dark_gray", "orange"),
-  'distinguish2' = get_hurwitz_colors("viking", "gold", "dark_gray", "orange"),
-  'shallow_ocean' = get_hurwitz_colors("gold", "tan", "light_gray", "viking"),
-  'deep_ocean' = get_hurwitz_colors("teal", "dark_gray", "brown", "light_gray"),
-  'contrast' = get_hurwitz_colors("teal", "gold", "red")
+  "main"  = get_hurwitz_colors("teal", "dark_gray", "orange"),
+  "classic"  = get_hurwitz_colors("teal", "orange"),
+  "gray"  = get_hurwitz_colors("light_gray", "dark_gray"),
+  "distinguish" = get_hurwitz_colors("teal", "gold", "dark_gray", "orange"),
+  "distinguish2" = get_hurwitz_colors("viking", "gold", "dark_gray", "orange"),
+  "shallow_ocean" = get_hurwitz_colors("gold", "tan", "light_gray", "viking"),
+  "deep_ocean" = get_hurwitz_colors("teal", "dark_gray", "brown", "light_gray"),
+  "contrast" = get_hurwitz_colors("teal", "gold", "red")
 )
 
 #' Print hex codes of colors in a palette
@@ -62,8 +62,8 @@ hurwitz_palettes <- list(
 #'
 #' @export
 hurwitz_pal <- function(name) {
-  if(!(name %in% names(hurwitz_palettes))){
-    stop(paste(name,"is not a valid palette name for hurwitz_palettes\n"))
+  if (!(name %in% names(hurwitz_palettes))) {
+    stop(paste(name, "is not a valid palette name for hurwitz_palettes\n"))
   }
 
   return(hurwitz_palettes[[name]])
@@ -78,13 +78,14 @@ hurwitz_pal <- function(name) {
 display_hurwitz_pal <- function(name, n = length(hurwitz_palettes[[name]])) {
   pal_colors <- hurwitz_pal(name)
 
-  if(n<2){
-    warning("minimal value for n is 2, displaying requested palette with 2 different levels\n")
+  if (n < 2) {
+    warning("minimal value for n is 2, displaying requested
+            palette with 2 different levels\n")
     return(display_hurwitz_pal(name, 2))
   }
-  if(n > length(hurwitz_palettes[[name]])){
+  if (n > length(hurwitz_palettes[[name]])) {
     warning(paste("n too large, allowed maximum for palette",
-                  name,"is",length(hurwitz_palettes[[name]])),
+                  name, "is", length(hurwitz_palettes[[name]])),
             "\nDisplaying the palette you asked for with that many colors\n")
     return(display_hurwitz_pal(name, length(hurwitz_palettes[[name]])))
   }
@@ -121,14 +122,17 @@ get_hurwitz_pal <- function(palette = "all", reverse = FALSE, ...) {
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'            scale_color_gradientn(), used respectively when discrete is
+#'            TRUE or FALSE
 #'
 #' @export
-scale_color_hurwitz <- function(palette = "all", discrete = TRUE, reverse = FALSE, ...) {
+scale_color_hurwitz <- function(palette = "all", discrete = TRUE,
+                                reverse = FALSE, ...) {
   pal <- get_hurwitz_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
-    ggplot2::discrete_scale("colour", paste0("hurwitz_", palette), palette = pal, ...)
+    ggplot2::discrete_scale("colour", paste0("hurwitz_", palette),
+                            palette = pal, ...)
   } else {
     ggplot2::scale_color_gradientn(colours = pal(256), ...)
   }
@@ -140,7 +144,8 @@ scale_color_hurwitz <- function(palette = "all", discrete = TRUE, reverse = FALS
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'            scale_color_gradientn(), used respectively when discrete is
+#'            TRUE or FALSE
 #'
 #' @export
 scale_colour_hurwitz <- scale_color_hurwitz
@@ -151,14 +156,17 @@ scale_colour_hurwitz <- scale_color_hurwitz
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'            scale_fill_gradientn(), used respectively when discrete is
+#'            TRUE or FALSE
 #'
 #' @export
-scale_fill_hurwitz <- function(palette = "all", discrete = TRUE, reverse = FALSE, ...) {
+scale_fill_hurwitz <- function(palette = "all", discrete = TRUE,
+                               reverse = FALSE, ...) {
   pal <- get_hurwitz_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
-    ggplot2::discrete_scale("fill", paste0("hurwitz_", palette), palette = pal, ...)
+    ggplot2::discrete_scale("fill", paste0("hurwitz_", palette),
+                            palette = pal, ...)
   } else {
     ggplot2::scale_fill_gradientn(colours = pal(256), ...)
   }
